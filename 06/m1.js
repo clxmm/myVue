7c310a622f9f28dd961e68b3dd2568ee44004bc1
clxmm <meng_yme@163.com>
2020-07-22 21:22:26 +0800

    06

---
+++ 06/m1.js
@@ -0,0 +1,18 @@
+let a = 10
+let c = 20
+let d = 30
+
+function show() {
+    console.log("1111")
+}
+
+export let s1  = 'aaa'
+export let s2 = 'ccc'
+export let s3 = 'ddd'
+export function say() {
+    console.log("hello");
+}
+
+export default {
+    a,c,show
+}

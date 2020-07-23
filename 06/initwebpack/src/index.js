d789644ebcdea497d09cd38e1019af5c039cd37e
clxmm <meng_yme@163.com>
2020-07-23 21:42:55 +0800

    webpack的初步使用

---
+++ 06/initwebpack/src/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery'
+import './css/1.css'
+import './css/1.less'
+
+$(function() {
+    $('li:odd').css('backgroundColor','pink');
+    $('li:even').css('backgroundColor','red');
+
+
+})
---
+++ 06/initwebpack/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html', 
+    filename: 'index.html'
+});
+
+module.exports = {
+    //编译模式
+    mode: 'development',  // development   production
+    entry: path.join(__dirname,'./src/index.js'), //入口文件
+    output: {
+        path: path.join(__dirname,'./dist'),//输出文件的存放路径
+        filename: 'bundle.js'  //输出文件的名称
+    },
+    plugins: [htmlPlugin],
+
+     module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [ 'style-loader', 'css-loader' ]
+                },
+                { 
+                    test: /\.less$/, 
+                    use: ['style-loader', 'css-loader', 'less-loader'] 
+                
+                }
+            ]
+    }
+}
